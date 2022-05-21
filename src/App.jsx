ae3a1bbacbab97f856659919362404389294e5b6
iwashiro <iwashiro0517@yahoo.co.jp>
2022-05-22 01:28:30 +0900

    add dft function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState ,useRef} from "react";
+import { useEffect, useState, useRef } from "react";
 import { CSVLink } from "react-csv";
 import { Line } from "react-chartjs-2";
-import { Chart,registerables } from 'chart.js'
+import { Chart, registerables } from 'chart.js'
 
 Chart.register(...registerables)
 
@@ -10,31 +10,31 @@ export const App = () => {
     const isActive = useRef(false);
     const startTime = useRef(0);
     const curAcc = useRef({
-        t : [],
-        x : [],
-        y : [],
-        z : [],
+        t: [],
+        x: [],
+        y: [],
+        z: [],
     });
-    const [isArrow,setIsArrow] = useState(false);
-    const [measurementTime,setMeasurementTime] = useState(0);
-    const [resultAcc,setResultAcc] = useState(null);
+    const [isArrow, setIsArrow] = useState(false);
+    const [measurementTime, setMeasurementTime] = useState(0);
+    const [resultAcc, setResultAcc] = useState(null);
 
     const getAcceleration = (event) => {
-        if (isTouch.current){
-            if (!isActive.current){
+        if (isTouch.current) {
+            if (!isActive.current) {
                 startTime.current = Date.now();
                 isActive.current = true;
-                return 
+                return
             }
-            const {x:newX,y:newY,z:newZ} = event.acceleration;
-            const {t,x,y,z} = curAcc.current
+            const { x: newX, y: newY, z: newZ } = event.acceleration;
+            const { t, x, y, z } = curAcc.current
             const newT = (Date.now() - startTime.current) / 1000;
-            curAcc.current = {t:[...t,newT],x:[...x,newX],y:[...y,newY],z:[...z,newZ]};
-        }else{
-            if (isActive.current){
+            curAcc.current = { t: [...t, newT], x: [...x, newX], y: [...y, newY], z: [...z, newZ] };
+        } else {
+            if (isActive.current) {
                 setResultAcc(curAcc.current);
-                setMeasurementTime( (Date.now() - startTime.current) / 1000);
-                curAcc.current = {t:[],x:[],y:[],z:[]};
+                setMeasurementTime((Date.now() - startTime.current) / 1000);
+                curAcc.current = { t: [], x: [], y: [], z: [] };
                 isActive.current = false;
             }
         }
@@ -45,87 +45,224 @@ export const App = () => {
     }
 
     const checkDialog = () => {
-        if(navigator.userAgent.match('iPhone|iPad')){
-        window.DeviceMotionEvent.requestPermission().then((res) => {
-            if (res === 'granted') {
-                window.addEventListener('devicemotion',getAcceleration);
-                window.addEventListener('deviceorientation',getGyro);
-                setIsArrow(true);
-            } else {
-                alert('ブラウザを再起動してセンサー利用の許可をして下さい！');
-                return 
-            }
-        })
-    }else{
-        setIsArrow(true);
-    }
-        
+        if (navigator.userAgent.match('iPhone|iPad')) {
+            window.DeviceMotionEvent.requestPermission().then((res) => {
+                if (res === 'granted') {
+                    window.addEventListener('devicemotion', getAcceleration);
+                    window.addEventListener('deviceorientation', getGyro);
+                    setIsArrow(true);
+                } else {
+                    alert('ブラウザを再起動してセンサー利用の許可をして下さい！');
+                    return
+                }
+            })
+        } else {
+            setIsArrow(true);
+        }
+
     }
 
     const renderCsvBtn = () => {
-        const {t,x,y,z} = resultAcc
+        const { t, x, y, z } = resultAcc
         const data = [
-            ['time','x_acc','y_acc','z_acc'],
-            ...resultAcc.x.map((el,i) => {
-                return [t[i],x[i],y[i],z[i]]
+            ['time', 'x_acc', 'y_acc', 'z_acc'],
+            ...resultAcc.x.map((el, i) => {
+                return [t[i], x[i], y[i], z[i]]
             })
         ];
         return <CSVLink filename='Acceleration_data.csv' data={data}>Download CSV</CSVLink>
     }
 
     const renderChart = () => {
+        const N = resultAcc.t.length;
+        const dt = resultAcc.t[1] - resultAcc.t[0];
+        const x = dft(resultAcc.x);
+        const y = dft(resultAcc.y);
+        const z = dft(resultAcc.z);
+        const dftData = {
+            freq: [...Array(N)].map((_, i) => (i * (1 / dt) / N).toFixed(2)).slice(0, Math.floor(N / 2) + 1),
+            x: x.Re.map((_, index) => Math.sqrt(x.Re[index] ** 2 + x.Im[index] ** 2) / N * 2).slice(0, Math.floor(N / 2) + 1),
+            y: y.Re.map((_, index) => Math.sqrt(y.Re[index] ** 2 + y.Im[index] ** 2) / N * 2).slice(0, Math.floor(N / 2) + 1),
+            z: z.Re.map((_, index) => Math.sqrt(z.Re[index] ** 2 + z.Im[index] ** 2) / N * 2).slice(0, Math.floor(N / 2) + 1),
+        }
+        const target = dftData.z;
+        const maxAmpIndex = target.indexOf(target.reduce((prev, cur) => Math.max(prev, cur)));
+        const peakFreq = dftData.freq[maxAmpIndex];
+
         const data = {
             labels: resultAcc.t,
             datasets: [
-              {
-               label:'x',
-               data:resultAcc.x,
-               borderColor:'green'
-              },
-              {
-                label:'y',
-                data:resultAcc.y,
-                borderColor:'red'
-              },
-              {
-                  label:'z',
-                  data:resultAcc.z,
-                  borderColor:'black'
+                {
+                    label: 'x',
+                    data: resultAcc.x,
+                    borderColor: 'green'
+                },
+                {
+                    label: 'y',
+                    data: resultAcc.y,
+                    borderColor: 'red'
+                },
+                {
+                    label: 'z',
+                    data: resultAcc.z,
+                    borderColor: 'black'
 
-              } 
+                }
             ],
+        }
+        const data2 = {
+            labels: dftData.freq,
+            datasets: [
+                {
+                    label: 'x',
+                    data: dftData.x,
+                    borderColor: 'green'
+                },
+                {
+                    label: 'y',
+                    data: dftData.y,
+                    borderColor: 'red'
+                },
+                {
+                    label: 'z',
+                    data: dftData.z,
+                    borderColor: 'black'
+                },
+            ]
+        }
+
+        const baseOptions = {
+            elements: {
+                point: {
+                    radius: 0
+                },
+            },
+        }
+
+        const options1 = {
+            ...baseOptions, ...{
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Acceleration'
+                    }
+                },
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Times [s]',
+                            color:'black'
+
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Acceleration [m/s^2]',
+                            color:'black'
+                        }
+                    }
+                }
+            }
+        }
+
+        const options2 = {
+            ...baseOptions, ...{
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Frequency'
+                    }
+                },
+                scales: {
+                    x: {
+
+                        title: {
+                            display: true,
+                            text: 'Frequency [Hz]',
+                            color:'black'
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Amplitude',
+                            color:'black'
+                        }
+                    }
+                }
+            }
         }
 
         return (
             <div>
-              <Line data={data} />
+                <div style={{ border: 'solid 1px' }}>
+                    <Line data={data} options={options1} height={500}/>
+                </div>
+                <div style={{ border: 'solid 1px', marginTop: '1em' }}>
+                    <Line data={data2} options={options2} />
+                </div>
+                <p>ピーク周波数 {peakFreq}</p>
+                <p>ピーク周期 {1 / peakFreq}</p>
             </div>
         )
-}
+    }
+
+    const dft = (array) => {
+        const N = array.length;
+        let Re = [];
+        let Im = [];
+        [...Array(N)].map((_, time) => {
+            const Re_sum = array.map((fx, x) => fx * Math.cos(2 * Math.PI * time * x / N)).reduce((prev, cur) => prev + cur);
+            const Im_sum = array.map((fx, x) => -fx * Math.sin(2 * Math.PI * time * x / N)).reduce((prev, cur) => prev + cur);
+            Re = [...Re, Re_sum];
+            Im = [...Im, Im_sum];
+        })
+
+        return { Re: Re, Im: Im }
+    }
+
+    const idft = (array) => {
+        const N = array.length;
+        let Re = [];
+        let Im = [];
+        [...Array(N)].map((_, time) => {
+            const Re_sum = array.map((fx, x) => fx * Math.cos(2 * Math.PI * time * x / N)).reduce((prev, cur) => prev + cur);
+            const Im_sum = array.map((fx, x) => fx * Math.sin(2 * Math.PI * time * x / N)).reduce((prev, cur) => prev + cur);
+            Re = [...Re, Re_sum];
+            Im = [...Im, Im_sum];
+        })
+        return { Re: Re.map(el => el / N), Im: Im.map(el => el / N) }
+    }
 
     const buttonStyle = {
-        width:300,
-        height:300,
-        fontSize:30
+        width: 300,
+        height: 300,
+        fontSize: 30
     }
 
-    if (!DeviceOrientationEvent.requestPermission && !navigator.userAgent.match('Android.+Mobile')){
-        return <p>pcでは利用できません</p>
-    }
-
-    if (!isArrow){
+    if (!DeviceOrientationEvent.requestPermission && !navigator.userAgent.match('Android.+Mobile')) {
         return (
-        <button onClick={checkDialog}>センサーの有効化</button>
+            <>
+                <p>pcでは利用できません</p>
+            </>
+        )
+    }
+
+    if (!isArrow) {
+        return (
+            <button onClick={checkDialog}>センサーの有効化</button>
         )
     }
 
     return (
         <>
-            <div style={{textAlign:'center',marginTop:100}}>
-                <p><button style={buttonStyle} 
-                           onTouchEnd={() => isTouch.current=false} 
-                           onTouchStart={() => isTouch.current=true}>
-                            長押しで計測</button></p>
+            <div style={{ textAlign: 'center', marginTop: 100 }}>
+                <p><button style={buttonStyle}
+                    onTouchEnd={() => isTouch.current = false}
+                    onTouchStart={() => isTouch.current = true}>
+                    長押しで計測</button></p>
                 {resultAcc ? renderChart() : null}
                 <p>計測時間 : {measurementTime} [sec]</p>
                 <p>{resultAcc ? renderCsvBtn() : null}</p>
